3b80b1e28dcb0e93362889cbe2657c2eedb2f02e
Eli Barbes <64696257+barbes123@users.noreply.github.com>
2026-02-11 18:13:46 +0200

    added password for host

--- constants.ts
+++ constants.ts
@@ -2,3 +2,5 @@ export const SOUNDS = {
 //   WINNER_FANFARE: 'https://assets.mixkit.co/active_storage/sfx/2013/2013-preview.mp3', // Replace with your actual path
   WINNER_FANFARE: '/music/um_buzz.mp3',
 };
+
+export const HOST_PASSWORD = '1234'
